de073e353de5462f6a0aaf246ef584c649b42e29
tangjz <36634584@qq.com>
2022-03-25 20:38:13 +0800

    add hu

---
+++ hu/target/debug/deps/hu-8a8801c420399269.d
@@ -0,0 +1,5 @@
+e:\tangjzwork\rust\huhd-tools\hu\target\debug\deps\hu-8a8801c420399269.rmeta: src\main.rs
+
+e:\tangjzwork\rust\huhd-tools\hu\target\debug\deps\hu-8a8801c420399269.d: src\main.rs
+
+src\main.rs:
